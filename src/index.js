7a10df2c59947a297514e827e5026118a2e1145a
Madhuri Gubbala <gmadhuri@ucsd.edu>
2018-10-15 19:17:22 -0700

    Use arrow function

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 class Square extends React.Component {
     render() {
         return (
-            <button className="square" onClick={function () {
+            <button className="square" onClick={() => {
                 alert('click');
             }}>
                 {this.props.value}
